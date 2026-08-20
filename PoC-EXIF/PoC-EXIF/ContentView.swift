7140f78c38c00bc6160de2e9e28b6dbe611856d7
eemdeeks <prince9088@gmail.com>
2026-08-20 16:23:35 +0900

    poc: poc project init

---
+++ PoC-EXIF/PoC-EXIF/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PoC-EXIF
+//
+//  Created by 박승찬 on 8/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PoC-EXIF/PoC-EXIFTests/PoC_EXIFTests.swift
@@ -0,0 +1,17 @@
+//
+//  PoC_EXIFTests.swift
+//  PoC-EXIFTests
+//
+//  Created by 박승찬 on 8/20/26.
+//
+
+import Testing
+@testable import PoC_EXIF
+
+struct PoC_EXIFTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
