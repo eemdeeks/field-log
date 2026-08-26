243ef8d60f1b88174309b4d36cf16b73b2438783
eemdeeks <prince9088@gmail.com>
2026-08-26 18:49:06 +0900

    chore: 프로젝트 생성

---
+++ FieldLog/FieldLog/FieldLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  FieldLogApp.swift
+//  FieldLog
+//
+//  Created by 박승찬 on 8/26/26.
+//
+
+import SwiftUI
+
+@main
+struct FieldLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FieldLog/FieldLogTests/FieldLogTests.swift
@@ -0,0 +1,19 @@
+//
+//  FieldLogTests.swift
+//  FieldLogTests
+//
+//  Created by 박승찬 on 8/26/26.
+//
+
+import Testing
+@testable import FieldLog
+
+struct FieldLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
